ed40bc1f3c8137a3bc442bd3000cb7bc5e206fb4
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-03-12 15:45:26 -0700

    take and drop items

--- src/item.py
+++ src/item.py
@@ -1,4 +1,15 @@
 class Item:
+
     def __init__(self, name, description):
         self.name = name
         self.description = description
+
+    def __str__(self):
+        string = f"items: {self.name}, {self.description}"
+        return string
+
+    def on_take(self):
+        print(f"You have picked up {self.name}")
+
+    def on_drop(self):
+        print(f"You have dropped {self.name}")
